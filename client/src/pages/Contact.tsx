51b7ed7488424a25f57161ff2fa5b98e2ffa0e3e
william <wayleemh@gmail.com>
2024-07-22 23:56:01 -0400

    client init home and contact pages

---
+++ client/src/pages/Contact.tsx
@@ -0,0 +1,10 @@
+
+function Contact() {
+  return (
+    <div>
+      <h1 className="text-primary text-6xl font-bold mb-6">support</h1>
+    </div>
+  )
+}
+
+export default Contact 
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <h1 className="text-primary text-8xl font-bold ml-32 mt-32 mb-6">heapstart</h1>
+    </div>
+  )
+}
+
+export default Home
