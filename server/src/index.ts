d32a68c07d1767541d66ba44f210b023634378fc
william <wayleemh@gmail.com>
2024-07-23 04:17:18 -0400

    server add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,31 @@
-import dotenv from "dotenv"
-import express from 'express';
-import cors from 'cors';
-import mongoose from 'mongoose';
+import dotenv from "dotenv";
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
 
-import { userRouter } from './routes/user';
+import { userRouter } from "./routes/user";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(
+	cors({
+		origin: "http://localhost:5173",
+		optionsSuccessStatus: 200,
+	}),
+);
 
-app.use("/user", userRouter)
+app.use("/user", userRouter);
 
-mongoose.connect(process.env.DB_URL, { dbName: process.env.DB_NAME }).then(() => {
-  console.log("Connected to MongoDB");
-  app.listen(3001, () => console.log("SERVER STARTED"));
-})
-  .catch(err => console.error("MongoDB connection error:", err));
-
+mongoose
+	.connect(process.env.MONGODB_URL, { dbName: "ecommerce" })
+	.then(() => {
+		console.log("Connected to MongoDB");
+		app.listen(3001, () => console.log("SERVER STARTED"));
+	})
+	.catch((err) => console.error("MongoDB connection error:", err));
 
 /*
 curl -X POST http://localhost:3001/user/register \
